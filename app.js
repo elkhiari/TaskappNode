c80101d14738226d12694847a7395b6494a76516
Othmane Elkhiari <oelkhiar@e1r2p13.1337.ma>
2023-01-14 16:09:48 +0100

    Create add items api

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+const tasks = require('./routes/task');
+
+const connectDB = require('./db/connect')
+
+require('dotenv').config()
+
+// middleware
+
+app.use(express.json())
+
+// Routes
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.use('/api/v1/tasks',tasks)
+
+
+
+
+
+const port = 3000;
+
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`server is listening on port ${port}...`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+start()
